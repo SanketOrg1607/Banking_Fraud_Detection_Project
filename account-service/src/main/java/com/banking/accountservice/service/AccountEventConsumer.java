7a71ac1fabe207b3280c6bc17afde4c88f70facd
sanket <sanketkhokade50@gmail.com>
2026-08-05 12:32:52 +0530

    Adding Event consumer methods for account service- consume transaction complete event and consume fraud detection event via kafka

--- account-service/src/main/java/com/banking/accountservice/service/AccountEventConsumer.java
+++ account-service/src/main/java/com/banking/accountservice/service/AccountEventConsumer.java
@@ -1,4 +1,56 @@
 package com.banking.accountservice.service;
 
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+@Slf4j
+@RequiredArgsConstructor
+@KafkaListener(topics="transaction.completed")
 public class AccountEventConsumer {
+
+    private final AccountService accountService;
+
+    /*
+    * Consume transaction completed event from  kafka
+    * credits receiver amount
+    * @param payload
+    * */
+    public void consumeTransactionCompleted(@Payload Map<String, Object> payload)
+    {
+        try{
+            String receiverAccount = (String)payload.get("receiverAccountNumber");
+            BigDecimal amount = new BigDecimal(payload.get("amount").toString());
+
+            log.info("Creating account {} : amount {}",receiverAccount,amount);
+            accountService.creditBalance(receiverAccount,amount);
+        }
+        catch(Exception e)
+        {
+            log.error("Error crediting account : {}",e.getMessage());
+        }
+    }
+
+    /*
+    * consume fraud detected event from kafka
+    * Blocks hre flagged account
+    * @param payload
+    */
+    public void consumerFraudDetected(@Payload Map <String,Object> payload)
+    {
+        try{
+            String accountNumber= (String)payload.get("accountNumber");
+            log.info("Fraud detected - blocking account : {}",accountNumber);
+
+            accountService.blockAccount(accountNumber);
+        }
+        catch(Exception e){
+            log.error("Error blocking account :{}",e.getMessage());
+        }
+    }
 }
